b5f45be65abf1b53e491ffd7ef31f7d1f687d21e
Furkan Balcı <105873821+furkanbalc@users.noreply.github.com>
2023-09-30 02:46:14 +0300

    Update config.dart

--- demo_todo_app/lib/service/config.dart
+++ demo_todo_app/lib/service/config.dart
@@ -1,4 +1,4 @@
-const url = 'http://192.168.251.89:3000/';
+const url = 'http://your-ipv4-adress:3000/';
 const createTodo = '${url}createTodo';
 const getTodos = '${url}getTodos';
 const deleteTodo = '${url}deleteTodo';
